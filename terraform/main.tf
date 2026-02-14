6e17cdc4ab28b3fbe5532287eb23bd4f0a8b1dcb
Abdulai <abdulaileb@gmail.com>
2026-02-14 22:09:20 +0100

    just fixed the terraform vpc and networking connection

--- terraform/main.tf
+++ terraform/main.tf
@@ -34,3 +34,40 @@ resource "aws_instance" "app_server" {
     Name = "learn-terraform"
   }
 }
+
+resource "aws_vpc" "default" {
+    cidr_block = "10.0.0.0/16"
+    tags = {
+        Name = "default-vpc"
+    }
+}
+
+
+resource "aws_internet_gateway" "default" {
+    vpc_id = aws_vpc.default.id
+    tags = {
+        Name = "default-igw"
+    }
+}
+
+resource "aws_subnet" "default" {
+    vpc_id = aws_vpc.default.id
+    cidr_block = "10.0.1.0/24"
+    availability_zone = "us-east-1a"
+    tags = {
+        Name = "default-subnet"
+    }
+}
+
+resource "aws_security_group" "allow_ssh" {
+    name = "allow_ssh"
+    description = "Allow SSH inbound traffic"
+    vpc_id = aws_vpc.default.id
+
+    ingress {
+        from_port = 22
+        to_port = 22
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+}
