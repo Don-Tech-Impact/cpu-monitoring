0d3f5187e709fbc381b1a1cdc1765ae515678913
Abdulai <abdulaileb@gmail.com>
2026-02-14 23:42:58 +0100

    fixed the issue cidr issue on the main terraform

--- terraform/main.tf
+++ terraform/main.tf
@@ -19,7 +19,7 @@ data "aws_ami" "ubuntu" {
   most_recent = true
 
   filter {
-    name = "name"
+    name   = "name"
     values = ["ubuntu/images/hvm-ssd-gp3/ubuntu-noble-24.04-amd64-server-*"]
   }
 
@@ -31,59 +31,129 @@ resource "aws_instance" "app_server" {
   instance_type = "t2.micro"
 
   tags = {
-    Name = "learn-terraform"
+    Name = "public_web-server-terraform"
   }
+
+  network_interface {
+    device_index         = 0
+    network_interface_id = aws_network_interface.web_server_eni.id
+  }
+
+  user_data = <<-EOF
+              #!/bin/bash
+              apt-get update -y
+              apt-get install -y nginx
+              systemctl start nginx
+              systemctl enable nginx
+              sudo echo "Hello, World from $(hostname -f)" > /var/www/html/index.html
+          EOF
 }
 
 resource "aws_vpc" "default" {
-    cidr_block = "10.0.0.0/16"
-    tags = {
-        Name = "default-vpc"
-    }
+  cidr_block = "10.0.0.0/16"
+  tags = {
+    Name = "default-vpc"
+  }
 }
 
 
 resource "aws_internet_gateway" "default" {
-    vpc_id = aws_vpc.default.id
-    tags = {
-        Name = "default-igw"
-    }
+  vpc_id = aws_vpc.default.id
+  tags = {
+    Name = "default-igw"
+  }
 }
 
 resource "aws_route_table" "default" {
-    vpc_id = aws_vpc.default.id
-    route {
-        cidr_block = aws_subnet.default.cidr_block
-        gateway_id = aws_internet_gateway.default.id
-    }
+  vpc_id = aws_vpc.default.id
+  route {
+    cidr_block = aws_subnet.default.cidr_block
+    gateway_id = aws_internet_gateway.default.id
+  }
 
-    route {
-        ipv6_cidr_block = "::/0"
-        gateway_id = aws_internet_gateway.default.id
-    }
-    tags = {
-        Name = "default-route-table-for prod"
-    }
-    
+  route {
+    ipv6_cidr_block = "::/0"
+    gateway_id      = aws_internet_gateway.default.id
+  }
+  tags = {
+    Name = "default-route-table-for prod"
+  }
+
 }
+
+resource "aws_route_table_association" "default" {
+  subnet_id      = aws_subnet.default.id
+  route_table_id = aws_route_table.default.id
+}
+
+
+
 resource "aws_subnet" "default" {
-    vpc_id = aws_vpc.default.id
-    cidr_block = "10.0.1.0/24"
-    availability_zone = "us-east-1a"
-    tags = {
-        Name = "default-subnet"
-    }
+  vpc_id            = aws_vpc.default.id
+  cidr_block        = "10.0.1.0/24"
+  availability_zone = "us-east-1a"
+  tags = {
+    Name = "default-subnet"
+  }
 }
 
-resource "aws_security_group" "allow_ssh" {
-    name = "allow_ssh"
-    description = "Allow SSH inbound traffic"
-    vpc_id = aws_vpc.default.id
+resource "aws_security_group" "allow_ssh_and_web" {
+  name        = "allow_ssh_and_web"
+  description = "Allow SSH and HTTP inbound traffic"
+  vpc_id      = aws_vpc.default.id
 
-    ingress {
-        from_port = 22
-        to_port = 22
-        protocol = "tcp"
-        cidr_blocks = ["0.0.0.0/0"]
-    }
+  ingress {
+    description = "Allow ssh"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "Allow HTTPS traffic"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "Allow HTTP traffic"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    description = "Allow all outbound traffic"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ### Netwrok interface 
+}
+
+resource "aws_network_interface" "web_server_eni" {
+  subnet_id       = aws_subnet.default.id
+  security_groups = [aws_security_group.allow_ssh_and_web.id]
+  tags = {
+    Name = "app-server-eni"
+  }
+}
+
+resource "aws_eip" "web_server_eip" {
+  domain = "vpc"
+  tags = {
+    Name = "app-server-eip"
+  }
+}
+
+resource "aws_eip_association" "web_server_eip_assoc" {
+  allocation_id        = aws_eip.web_server_eip.id
+  network_interface_id = aws_network_interface.web_server_eni.id
+  depends_on           = [aws_internet_gateway.default]
 }
